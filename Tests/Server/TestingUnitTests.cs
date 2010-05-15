335ab6f4e0315f02814874b923f9db724730045d
tbreivik <tbreivik@gmail.com>
2010-05-16 00:45:03 +0200

    Adde unit tests to test teamcity unit test runner

---
+++ Tests/Server/TestingUnitTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Server
+{
+    [TestClass]
+    public class TestingUnitTests
+    {
+
+        public void TestRunner()
+        {
+            Assert.Fail();
+        }
+    }
+}
