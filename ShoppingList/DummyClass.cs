c89bf72776ba2d96d8eccd07fc9037e89cd8037f
tbreivik <tbreivik@gmail.com>
2010-05-16 01:59:36 +0200

    Silverlight test

---
+++ ShoppingList/DummyClass.cs
@@ -0,0 +1,7 @@
+namespace ShoppingList
+{
+    public class DummyClass
+    {
+        
+    }
+}
---
+++ Tests/Silverlight/ShoppingList/Tests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ShoppingList
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.Fail("Failing");
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+        }
+    }
+}
